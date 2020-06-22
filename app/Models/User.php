68975c596dab409f099fd7d5c4429b282a314ac7
Elisandro Mello <elisandro@ulbra.edu.br>
2020-06-21 23:04:54 -0300

    refactor: update model user for primary key string

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,8 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class User extends Model
 {
-    public $timestamps = false;
+    public $timestamps      = false;
+    protected $table        = 'users';
+    protected $keyType      = 'string';
+    protected $primaryKey   = 'id';
+    public $incrementing    = false;
+    protected $casts = ['id' => 'string']; // cast
+    
     protected $fillable = [
-        "name"
+        'id'
     ];
+
 }
