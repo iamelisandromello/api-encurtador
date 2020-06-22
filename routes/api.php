3812b9e511d5aa9790316ac3d79e63255bc8314f
Elisandro Mello <elisandro@ulbra.edu.br>
2020-06-21 23:01:41 -0300

    feat: add routes to procprocess of requests

--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/* Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-}); */
+Route::get("urls/{url}", "UrlsController@link");                       //GET /urls/:id
+Route::post("users/{userid}/urls", "UrlsController@addUrl");           //POST /users/:userid/urls
+Route::get("stats", "UrlsController@index");                           //GET /stats
+Route::get("users/{userId}/stats", "UrlsController@statsByUser");      //GET /users/:userId/stats
+Route::get("stats/{url}", "UrlsController@url");                       //GET /stats/:id
+Route::delete("urls/{url}", "UrlsController@destroy");                 //DELETE /urls/:id
+Route::post("users", "UsersController@addUser");                       //POST /users
+Route::delete("user/{userId}", "UsersController@destroy");             //DELETE /user/:userId
 
-Route::get("urls", "UrlsController@index");
-Route::post("urls", "UrlsController@store");
-Route::post("users", "UsersController@addUser");
