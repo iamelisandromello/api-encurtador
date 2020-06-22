0271950fe07fdec83cf38331c85d2c32145175b1
Elisandro Mello <elisandro@ulbra.edu.br>
2020-06-21 23:03:14 -0300

    feat: add trait class uuid for treatment of short url

---
+++ app/Traits/Uuid.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+trait Uuid
+{
+    /**
+    * Change model primary key.
+    *
+    * @see Model::bootIfNotBooted()
+    *s
+    * @return void
+    */
+    protected function bootIfNotBooted()
+    {
+        parent::bootIfNotBooted();
+
+        $this->keyType = 'string';
+        $this->incrementing = false;
+    }
+
+    /**
+    * Boot UUID trait.
+    *
+    * @return void
+    */
+    public static function bootUuid()
+    {
+        self::creating(function ($model) {
+            $model->short_url = $model->generateUuid();
+        });
+    }
+
+    /**
+    * Generate a new UUID.
+    *
+    * @return string
+    */
+    public function generateUuid()
+    {
+        return substr((string) Str::uuid(), 0, 8);
+    }
+  }
