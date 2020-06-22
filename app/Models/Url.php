0124fcb3ef2a7a9efe086ca72f1462f32ba8d9c6
Elisandro Mello <elisandro@ulbra.edu.br>
2020-06-21 23:05:52 -0300

    refactor: update model url for use traits uuid

--- app/Models/Url.php
+++ app/Models/Url.php
@@ -4,12 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+use App\Traits\Uuid;
+
 class Url extends Model
 {
+    use Uuid;
     public $timestamps = false;
     protected $fillable = [
         "hit",
         "url",
-        "short-url"
+        "short_url",
+        "user_id"
     ];
 }
